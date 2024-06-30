8bdeef41b98b51276b2e70881537252da50d495a
KutayGuler <s7019@horyzont.eu>
2024-06-30 16:25:20 +0200

    added base, primary and secondary colors to tailwind config"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-full bg-base-900 p-4 text-white">
+	<div class="flex flex-row gap-2">
+		Base
+		<div class="size-8 rounded-full bg-base-500"></div>
+	</div>
+	<div class="flex flex-row gap-2">
+		Primary
+		<div class="size-8 rounded-full bg-primary-500"></div>
+	</div>
+	<div class="flex flex-row gap-2">
+		Secondary
+		<div class="size-8 rounded-full bg-secondary-500"></div>
+	</div>
+</div>
